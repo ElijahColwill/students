ca4a22a17fda76fa11d91ef0481af752233bf5c2
colwiller <colwiller@s.dcsdk12.org>
2017-09-08 11:16:49 -0600

    Implement function to determine average height of students and refactor sophomore_vowels

--- students.rb
+++ students.rb
@@ -1,12 +1,15 @@
 students = []
 eye_colors = []
 ages = []
+heights = []
 
 File.open("student_data.csv").each do |line|
 	info = line.split(",")
 	students.push(info[0].strip)
 	eye_colors.push(info[1].strip)
 	ages.push(info[2].strip.to_i)
+	height = (info[3].strip.to_i * 12) + info[4].strip.to_i
+	heights.push(height)
 end
 
 def brown_eyes(eye_colors)
@@ -50,17 +53,27 @@ end
 puts "Which girls have green eyes?"
 puts green_eyed_girls(students, eye_colors)
 
+def num_of_vowels(string)
+	vowels = 0 
+	string.chars.each do |char|
+		if is_vowel?(char)
+			vowels += 1
+		end
+	end
+	return vowels
+end
+
+def is_vowel?(char)
+	return char.downcase == "a" || char.downcase == "e" || char.downcase == "i" || char.downcase == "o" || char.downcase == "u" 
+end
+
 def sophomore_vowels(students, ages)
 	most_vowels = students[0]
 	max_vowels = 0
 	students.each_with_index do |current_name, i|
 		vowels = 0
 		if ages[i] == 15
-			current_name.chars.each do |char|
-				if char.downcase == "a" || char.downcase == "e" || char.downcase == "i" || char.downcase == "o" || char.downcase == "u" 
-					vowels += 1
-				end 
-			end
+			vowels = num_of_vowels(current_name)
 			if vowels > max_vowels
 				max_vowels = vowels
 				most_vowels = current_name
@@ -70,5 +83,17 @@ def sophomore_vowels(students, ages)
 	return most_vowels
 end
 
+
 puts "Which sophomore has the most vowels in their name?"
 puts sophomore_vowels(students, ages)
+
+def average_height(heights)
+	sum = 0
+	heights.each do |height|
+		sum += height
+	end
+	return (sum / heights.length)
+end
+
+puts "What is the average height of the students in inches?"
+puts average_height(heights)
